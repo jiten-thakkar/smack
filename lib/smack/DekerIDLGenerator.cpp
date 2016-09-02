a52c91cacfa1abf0d75035284d40e8ebb1c9e9b1
Jiten <jitenmt@gmail.com>
2016-09-02 05:24:29 +0000

    Added functionality to print read and write information recursively about parameters

--- lib/smack/DekerIDLGenerator.cpp
+++ lib/smack/DekerIDLGenerator.cpp
@@ -20,7 +20,7 @@
 #include <vector>
 
 struct Hash {
-  size_t operator() (const std::vector<unsigned int> &offsets) const {
+  size_t operator() (const std::vector<unsigned> &offsets) const {
     size_t hash = 0;
     for(auto it = offsets.begin(); it != offsets.end(); it++) {
       hash = hash*10+*it;
@@ -30,9 +30,9 @@ struct Hash {
 };
 
 struct offSets {
-  std::unordered_set<std::vector<unsigned int>, Hash> read;
-  std::unordered_set<std::vector<unsigned int>, Hash> write;
-  offSets(std::vector<unsigned int> offsets, bool isRead) {
+  std::unordered_set<std::vector<unsigned>, Hash> read;
+  std::unordered_set<std::vector<unsigned>, Hash> write;
+  offSets(std::vector<unsigned> offsets, bool isRead) {
     if(isRead)
       read.insert(offsets);
     else
@@ -66,6 +66,24 @@ namespace smack {
 
 using namespace llvm;
 
+void printOffsets(DSNode* node, std::string indentation) {
+          errs() << indentation << "Write\n";
+          for(DSNode::const_offset_iterator it=node->write_offset_begin(); it!=node->write_offset_end(); it++) {
+            errs() << indentation << *it << " ";
+          }
+          errs() << "\n";
+          errs() << indentation << "Read\n";
+          for(DSNode::const_offset_iterator it=node->read_offset_begin(); it!=node->read_offset_end(); it++) {
+            errs() << indentation << *it << " ";
+          }
+          errs() << "\n";
+          indentation.append("  ");
+          for(DSNode::const_edge_iterator ei=node->edge_begin(); ei!=node->edge_end(); ei++) {
+            errs() << indentation << "offset: " << (*ei).first << "\n";
+            printOffsets((*ei).second.getNode(), indentation);
+          }
+}
+
 bool DekerIDLGenerator::runOnModule(Module& m) {
   BU = &getAnalysis<BUDataStructures>();
   //TD = &getAnalysis<TDDataStructures>();
@@ -78,6 +96,7 @@ bool DekerIDLGenerator::runOnModule(Module& m) {
       std::vector<DSNode*> argumentNodes;
       int paramNumber=0;
       for (Function::arg_iterator A=F.arg_begin(); A!=F.arg_end(); A++, paramNumber++) {
+        errs() << "parameter: " << paramNumber << "\n";
 	Value* val = dyn_cast<Value>(&*A);
         if(!val || !val->getType()->isPointerTy()) {/*errs() << "got null \n"; */
 	  argumentNodes.push_back(NULL);
@@ -86,34 +105,44 @@ bool DekerIDLGenerator::runOnModule(Module& m) {
           DSNode *node = nodeHandle.getNode();
           argumentNodes.push_back(node);
           std::vector<unsigned int> writeOffsets;
+          errs() << "Write\n";
           for(DSNode::const_offset_iterator it=node->write_offset_begin(); it!=node->write_offset_end(); it++) {
             if(projections.find(paramNumber) == projections.end()) {
               projections[paramNumber] = offSets();
             } 
             writeOffsets.push_back(*it);
+            errs() << *it << " ";
           }
           projections[paramNumber].write.insert(writeOffsets);
-          
+          errs() << "\n";
           std::vector<unsigned int> readOffsets;
+          errs() << "Read\n";
           for(DSNode::const_offset_iterator it=node->read_offset_begin(); it!=node->read_offset_end(); it++) {
             if(projections.find(paramNumber) == projections.end()) {
               projections[paramNumber] = offSets();
             }
             readOffsets.push_back(*it);
+            errs() << *it << " ";
           }
           projections[paramNumber].read.insert(readOffsets);
+          errs() << "\n";
+          std::string indentation("  ");
+          //errs() << "num of links: " << node->getNumLinks()  << "\n";
           for(DSNode::const_edge_iterator ei=node->edge_begin(); ei!=node->edge_end(); ei++) {
             //if((*ei).second) {
-              errs() << "offset: " << (*ei).first << "\n";
-            //}
+              //errs() << "write: : " << (*ei).second.
+              //errs() << "parameter " << paramNumber << "offset: " << (*ei).first << "\n";
+          //  }
+            errs() << indentation << "offset: " << (*ei).first << "\n";
+            printOffsets((*ei).second.getNode(), indentation);
           }
 	}
       }
-              for(auto it = projections.begin(); it != projections.end(); it++) {
+              /*for(auto it = projections.begin(); it != projections.end(); it++) {
                 errs() << "Parameter: " << (*it).first << "\n";
                 (*it).second.print(errs());
                 errs() << "\n";
-              }
+              }*/
     }
   }
  return false;  
